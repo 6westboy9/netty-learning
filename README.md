31e447fe0bfa8797014934995357e4005f2afc7f
6westboy9 <542229163@qq.com>
2018-12-09 23:49:47 +0800

    添加 Netty 对 WebSocket 的支持

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 ## sheng-si-yuan-lecture
 
 1. Netty 第一个例子：模拟接受一个 HTTP 请求
+	* 需求说明：实现简单的一个 Web 服务器，当访问 Web 服务器时，返回 `Hello World` 字符串。 
 2. Netty 的 Socket 编程
+	* 需求说明：
 3. Netty 多客户端连接与通信
+	* 需求说明：
 4. Netty 读写检测机制与长连接
+	* 需求说明：
+5. Netty 对 WebSocket 的支持
+	* 需求说明： 
---
+++ sheng-si-yuan-lecture/src/main/java/com/westboy/netty/example05/WebSocketServer.java
@@ -0,0 +1,43 @@
+package com.westboy.netty.example05;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
+
+/**
+ * WebSocket 服务端
+ *
+ * @author westboy
+ * @date 2018-12-09
+ * @since 1.0
+ */
+public class WebSocketServer {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap
+                    .group(boosGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new WebSocketChannelInitializer());
+
+            // ChannelFuture channelFuture = serverBootstrap.bind(8899).sync(); 等效
+            ChannelFuture channelFuture = serverBootstrap.bind(new InetSocketAddress(8899)).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
